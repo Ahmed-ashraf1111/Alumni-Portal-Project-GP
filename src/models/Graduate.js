52980d694a8f0acc25ce975078e5c62059f89b40
Ahmed-ashraf1111 <129650864+Ahmed-ashraf1111@users.noreply.github.com>
2025-09-10 01:22:30 +0300

    Initial commit

---
+++ src/models/Graduate.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+const User = require('./User');
+
+const Graduate = sequelize.define('Graduate', {
+  graduate_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    references: {
+      model: User,
+      key: 'id'
+    }
+  },
+  bio: { type: DataTypes.STRING },
+  'linkedln-link': { type: DataTypes.STRING },
+  'current-job': { type: DataTypes.STRING, allowNull: true },
+  'cv-url': { type: DataTypes.STRING },
+  faculty: { type: DataTypes.STRING },
+  'profile-picture-url': { type: DataTypes.STRING },
+  'graduation-year': { type: DataTypes.INTEGER },
+  'skills': { type: DataTypes.STRING }
+}, {
+  tableName: 'Graduate',
+  timestamps: false
+});
+
+Graduate.belongsTo(User, { foreignKey: 'graduate_id' });
+
+module.exports = Graduate;
